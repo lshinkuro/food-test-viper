0be3cd26c5f8f119ced37dff40c3bc3c3c0b9e68
lshinkuro <kholis478@gmail.com>
2023-02-25 19:43:18 +0700

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Macbook on 25/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
